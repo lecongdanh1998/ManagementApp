ffc0e261d311fb1e5254b949a01eb7bfe6e01a73
Truong Nhat Nam <sunnam9897@gmail.com>
2018-10-23 15:18:19 +0700

    Revert "Add API login user"
    This reverts commit 84053e5

--- app/src/main/java/vn/edu/poly/managermentapp/Component/BaseActivity.java
+++ app/src/main/java/vn/edu/poly/managermentapp/Component/BaseActivity.java
@@ -1,62 +1,7 @@
 package vn.edu.poly.managermentapp.Component;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
-public class BaseActivity extends Activity {
+public class BaseActivity extends AppCompatActivity {
 
-    private final int MY_PERMISSIONS_REQUEST_INTERNET = 10;
-
-    public void checkInternet(Activity thisActivity) {
-
-        if (ContextCompat.checkSelfPermission(thisActivity,
-                Manifest.permission.INTERNET)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity,
-                    Manifest.permission.INTERNET)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                ActivityCompat.requestPermissions(thisActivity,
-                        new String[]{Manifest.permission.INTERNET},
-                        MY_PERMISSIONS_REQUEST_INTERNET);
-            }
-        } else {
-            // Permission has already been granted
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_INTERNET: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    finish();
-                    System.exit(0);
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request.
-        }
-    }
 }
--- app/src/main/java/vn/edu/poly/managermentapp/MainActivity.java
+++ app/src/main/java/vn/edu/poly/managermentapp/MainActivity.java
@@ -1,10 +1,9 @@
-package vn.edu.poly.managermentapp.View;
+package vn.edu.poly.managermentapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import vn.edu.poly.managermentapp.Component.BaseActivity;
-import vn.edu.poly.managermentapp.R;
 
 public class MainActivity extends BaseActivity {
 
